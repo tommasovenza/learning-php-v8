9154f1735e553754e14cb7a08b5a4dbf0c768674
tommasovenza <tommasovenza@gmail.com>
2025-11-11 16:11:38 +0100

    learning local and global scope

--- 16-scope/scope.php
+++ 16-scope/scope.php
@@ -1,3 +1,30 @@
-<?php 
+<?php
 
 // scope
+$globalVar = 10;
+
+// other global variable
+$otherGlobalVar = 200;
+
+function testGlobal()
+{
+
+    global $otherGlobalVar;
+
+    $globalVar = 13;
+
+    echo 'LOCAL SCOPE: ' .  $globalVar . PHP_EOL;
+
+    // dump GLOBALS array with all globals variables
+    var_dump($GLOBALS);
+
+    // dumping only $globalVar
+    var_dump($GLOBALS['globalVar']);
+
+    // now this var is accessible inside local scope
+    var_dump($otherGlobalVar);
+}
+
+testGlobal();
+
+echo 'GLOBAL SCOPE: ' .  $globalVar . PHP_EOL;
