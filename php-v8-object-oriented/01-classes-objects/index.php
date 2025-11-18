4c9cd5abb1056147b5ba090e5ec3c5f030ded715
tommasovenza <tommasovenza@gmail.com>
2025-11-18 11:26:42 +0100

    starting Object Oriented course's part

---
+++ php-v8-object-oriented/01-classes-objects/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes & Objects Index</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="php-v8-object-oriented/01-classes-objects">Classes & Objects</a></li>
+        <li><a href="php-v8-object-oriented/01-classes-objects">Classes & Objects MD</a></li>
+    </ul>
+</body>
+
+</html>
