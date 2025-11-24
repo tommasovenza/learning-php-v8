cdbeb263f4978b2ef861fee1ddeddffcb295a0d8
tommasovenza <tommasovenza@gmail.com>
2025-11-24 16:54:07 +0100

    create Gravity Interfaces and test it on two classes: Human and Telephone

--- php-v8-object-oriented/09-interfaces-and-polymorphism/interfaces-and-polymorphism.php
+++ php-v8-object-oriented/09-interfaces-and-polymorphism/interfaces-and-polymorphism.php
@@ -1,3 +1,62 @@
 <?php
 
-echo "interfaces-and-polymorphism";
+require_once "../Interfaces/Gravity.php";
+
+use App\Intefaces\Gravity;
+
+class Human implements Gravity
+{
+    public string $name;
+    public string $surname;
+
+    // create a new Human
+    public function __construct($name, $surname)
+    {
+        $this->name = $name;
+        $this->surname = $surname;
+    }
+
+    // Gravity func to implement
+    public function fallToTheGround(): string
+    {
+        return "I am forced by gravity..!";
+    }
+
+    // hello function
+    public function hello(): string
+    {
+        return "Hello from {$this->name}!";
+    }
+}
+
+class Telephone implements Gravity
+{
+    // property
+    public $telephoneName;
+
+    // create a new telephone item
+    public function __construct($telephoneName)
+    {
+        $this->telephoneName = $telephoneName;
+    }
+
+    // Gravity func to implement
+    public function fallToTheGround(): string
+    {
+        return "Craaack..! I'm Probably Broken!";
+    }
+
+    public function sound(): string
+    {
+        return "do do do...";
+    }
+}
+
+// create some entity
+$tommino = new Human('Tommaso', 'Venza');
+$iphone = new Telephone('Iphone');
+
+var_dump($tommino->hello()) . PHP_EOL;
+var_dump($tommino->fallToTheGround()) . PHP_EOL;
+var_dump($iphone->fallToTheGround()) . PHP_EOL;
+var_dump($iphone->sound()) . PHP_EOL;
---
+++ php-v8-object-oriented/Interfaces/Gravity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Intefaces;
+
+// create a Gravity Interface
+interface Gravity
+{
+    // no property or complicated stuff here...
+
+    // just a description of all methods 
+    // that classes need to implements 
+    // if they implement this interface
+    public function fallToTheGround(): string; // => all methods does not have any function body
+}
