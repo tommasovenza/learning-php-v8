5e0e701f385e10fc0c4c09827dfb0d375d0826df
tommasovenza <tommasovenza@gmail.com>
2025-11-25 17:11:42 +0100

    fix: restore User Class

--- php-v8-object-oriented/10-hierarchies-inheritance/hierarchies-and-inheritance.php
+++ php-v8-object-oriented/10-hierarchies-inheritance/hierarchies-and-inheritance.php
@@ -10,8 +10,6 @@ class AdminUser extends User
 {
     // static array so users persist between instances
     private static $adminUsers = [];
-    public string $role = 'ADMIN';
-    public static string $test = 'admin';
 
     // $name and $email are inherited from User Class
     public function __construct($name, $email)
@@ -26,14 +24,12 @@ class AdminUser extends User
         $this->storingAdminCredentials($this->name, $this->email);
     }
 
-    // simulating saving into database
     public function storingAdminCredentials($name, $email): void
     {
         echo "saving into database... {$name} - {$email}" . PHP_EOL;
         var_dump(self::$adminUsers) . PHP_EOL;
     }
 
-    // 
     public static function getAllAdminUsers(): string
     {
         $length = count(self::$adminUsers);
@@ -43,24 +39,12 @@ class AdminUser extends User
         return "admins found {$length}" . PHP_EOL;
     }
 }
-// testing!!
-$tommino = new AdminUser('tommino', 'tomminotest@gmail.com');
-$andrea = new AdminUser('andrea', 'andrea@gmail.com');
 
-var_dump($tommino->getPermissions()) . PHP_EOL;
-var_dump($andrea->role) . PHP_EOL;
+// testing!!
+$tommaso = new AdminUser('Tommino', 'tommio@gmail.com');
+
+var_dump($tommaso) . PHP_EOL;
 
 // get all admins from class
-$allAdmins = AdminUser::getAllAdminUsers();
-var_dump($allAdmins);
-
-// testing login and logout
-var_dump($tommino->login());
-var_dump($tommino->logout());
-var_dump($andrea->login());
-var_dump($andrea->logout());
-// 
-// $tommino->getTest();
-
-// new class
-// class ProjectManager extends User {}
+$allAdmin = AdminUser::getAllAdminUsers();
+var_dump($allAdmin);
