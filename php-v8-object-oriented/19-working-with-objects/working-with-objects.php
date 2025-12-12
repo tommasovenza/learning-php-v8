c36d4ea7df6e9c421ba5956212d1acbfd7213f63
tommasovenza <tommasovenza@gmail.com>
2025-12-12 11:21:15 +0100

    created People Class and two objects that pointing to same space's memory

--- php-v8-object-oriented/19-working-with-objects/working-with-objects.php
+++ php-v8-object-oriented/19-working-with-objects/working-with-objects.php
@@ -1,5 +1,30 @@
 <?php
 
-$var = "working with objects";
+// instantiate two objects that are identical after creating a People Class
 
-echo $var;
+class People
+{
+    // variables
+    public string $name;
+    public string $age;
+    public string $gender;
+
+    // construct
+    public function __construct(string $name, string $age, string $gender)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+    }
+}
+
+$person = new People('Tommaso', '42', 'male');
+$person2 = new People('Tommaso', '42', 'male');
+// identical object pointing to two different spaces in memory
+var_dump($person === $person2);
+
+// now space in memory will be the same
+$person = $person2;
+
+// so this var_dump will output => true
+var_dump($person === $person2); // true //
